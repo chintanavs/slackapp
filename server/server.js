1e5ee6f372c3df26c5752d52acaac95f18933705
Chintan Shah <chintan.avs@gmail.com>
2019-04-14 02:03:02 +0530

    final

--- server/server.js
+++ server/server.js
@@ -74,8 +74,7 @@ socket.on('createLocationMessage',(coords)=>{
 socket.on('disconnect',()=>{
   var user=users.removeUser(socket.id);
   if(user){
-    io.to(user.room).emit('updateUserList',users.getUserList(user.room));
-    io.to(user.room).emit('newMessage',generateMessage('Admin',user.name+' has left the chat'));
+  
   }
 });
 
